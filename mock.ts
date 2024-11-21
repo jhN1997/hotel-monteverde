33161063741835db85c9ddc6743cb135bdc6db5c
John Chunqui <john.chunqui@encora.com>
2024-11-21 16:01:50 -0500

    improve styles

--- mock.ts
+++ mock.ts
@@ -1,62 +1,56 @@
 export const mock = [
-    {
-      "name": "Porto Cristo, Mallorca",
-      "description": "Este hotel de categoría media está situado en Porto Cristo, a 10 minutos de la playa (unos 800 metros) y del centro de la localidad. En los alrededores del hotel podrá encontrar zonas comerciales con tiendas, boutiques, bares y restaurantes. A dos pasos están las famosas Cuevas del Drac. Este hotel, renovado en el año 2000, tiene un edificio principal de 7 plantas con un total de 7 habitaciones individuales, 113 dobles y 27 suites. El hotel dispone de un área de recepción abierta las 24 horas del día. Cuenta además con un bar, un pub, una sala de televisión y un restaurante. Podrá usar la terminal de Internet (bajo cargo adicional), el servicio de lavandería y el parque infantil. Las confortables habitaciones disponen de baño, teléfono de línea directa, caja fuerte, calefacción central y balcón o terraza. El hotel se enorgullece de su piscina al aire libre, chiringuito, pistas de tenis y programa de entretenimiento tanto para los más jóvenes como para los mayores. Se ofrece desayuno de bufet.",
-      "capacity": "1200",
-      "floorNumber": "15",
-      "adress": "Ctra. Cuevas de Drach, S/N,PORTO CRISTO 07680",
-      "photo": "https://photos.hotelbeds.com/giata/bigger/00/001857/001857a_hb_a_013.jpg",
-      "country": "España",
-      "id": "1"
-    },
-    {
-      "name": "Monteverde Boutique Hotel",
-      "description": "Monteverde Boutique Hotel tiene piscina al aire libre, jardín, salón de uso común y terraza en Pacasmayo. El alojamiento ofrece restaurante y también tiene bar. El alojamiento dispone de recepción 24 horas, traslado para ir o volver del aeropuerto, servicio de habitaciones y wifi gratis en todo el alojamiento.",
-      "capacity": "1020",
-      "floorNumber": "10",
-      "adress": " Av. alameda norte 222 - Jequetepeque, Pacasmayo",
-      "photo": "https://cf.bstatic.com/xdata/images/hotel/max1024x768/175877604.jpg?k=3a781e25c4b03409b983f3953e3507ade1b5c62c658c10547e377eace1cf0016&o=&hp=1",
-      "country": "Perú",
-      "id": "2"
-    },
-    {
-      "name": "Monteverde Country Lodge",
-      "description": "Este hotel ecológico ofrece habitaciones con WiFi gratuita y vistas a los jardines tropicales en Monteverde, Costa Rica, cerca del bosque nuboso de Monteverde. Las habitaciones del Monteverde Country Lodge están ubicadas en cabañas de madera con acceso a un porche compartido. Todas cuentan con TV por cable, cafetera, caja fuerte y baño con artículos de aseo gratuitos.",
-      "capacity": "860",
-      "floorNumber": "10",
-      "adress": "Monteverde Puntarenas, 60109 Monteverde",
-      "photo": "https://cf.bstatic.com/xdata/images/hotel/max1024x768/363309263.jpg?k=40dc63146678684296f74e5803175730a3fac60268dd3a16adfdbd378261019c&o=",
-      "country": "Costa Rica",
-      "id": "3"
-    },
-    {
-      "name": "Monteverde",
-      "description": "El Monteverde se encuentra en un lugar aislado de la región boscosa de Mazamitla y ofrece conexión Wi-Fi gratuita en las zonas comunes, instalaciones adaptadas para personas de movilidad reducida y gimnasio. Los alojamientos, de tipo chalé, son amplios y disponen de chimenea y zona de estar. También cuentan con terraza con vistas a los bosques, comedor y zona de cocina equipada. Todos ellos incluyen baño privado con artículos de aseo gratuitos.",
-      "capacity": "980",
-      "floorNumber": "10",
-      "adress": "Chavarria. Colonia Centro, 49500 Mazamitla",
-      "photo": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/e6/91/b7/cabanas-grandes.jpg?w=700&h=-1&s=1",
-      "country": "México",
-      "id": "4"
-    },
-    {
-      "name": "Monteverde Hotel La Dorada",
-      "description": "Monteverde Hotel está en La Dorada y tiene jardín, salón de uso común, bar y wifi gratis en todo el alojamiento. El hotel tiene piscina cubierta, piscina al aire libre y terraza, además de restaurante. En el hotel, las habitaciones cuentan con armario y TV de pantalla plana. Las habitaciones del alojamiento tienen baño privado con ducha y artículos de aseo gratuitos, y algunas habitaciones también cuentan con vistas a la piscina. Todas las habitaciones de este alojamiento tienen aire acondicionado y escritorio.",
-      "capacity": "1000",
-      "floorNumber": "10",
-      "adress": "Carrera 6 # 10-43 la dorada caldas, 175031 La Dorada",
-      "photo": "https://monteverde.hotelesejecafetero.net/data/Images/1920x1080w/15009/1500966/1500966451/image-la-dorada-monteverde-hotel-3.JPEG",
-      "country": "Colombia",
-      "id": "5"
-    },
-    {
-      "name": "Cabañas Monte Verde",
-      "description": "Las Cabañas Monte Verde se encuentran frente al lago Villarrica, a 5 km de Pucón, y ofrecen alojamiento independiente con calefacción y vistas al lago y a las montañas. Los bungalows de las Cabañas Monte Verde cuentan con baño privado, zona de cocina y TV. Las Cabañas Monte Verde tienen zona de barbacoa y servicio de información turística. Hay un jardín y una sala de juegos. Se puede aparcar en un parking privado gratuito de los alrededores.",
-      "capacity": "1010",
-      "floorNumber": "10",
-      "adress": "Camino Pucón - Villarrica Km 5, 4920000 Pucón",
-      "photo": "https://www.monteverdepucon.cl/wp-content/uploads/2019/01/facebook.jpg",
-      "country": "Chile",
-      "id": "6"
-    }
-  ]
+  {
+    "name": "Porto Cristo, Mallorca",
+    "description": "Este hotel de categoría media está situado en Porto Cristo, a 10 minutos de la playa (unos 800 metros) y del centro de la localidad. En los alrededores del hotel podrá encontrar zonas comerciales con tiendas, boutiques, bares y restaurantes. A dos pasos están las famosas Cuevas del Drac. Este hotel, renovado en el año 2000, tiene un edificio principal de 7 plantas con un total de 7 habitaciones individuales, 113 dobles y 27 suites. El hotel dispone de un área de recepción abierta las 24 horas del día. Cuenta además con un bar, un pub, una sala de televisión y un restaurante. Podrá usar la terminal de Internet (bajo cargo adicional), el servicio de lavandería y el parque infantil. Las confortables habitaciones disponen de baño, teléfono de línea directa, caja fuerte, calefacción central y balcón o terraza. El hotel se enorgullece de su piscina al aire libre, chiringuito, pistas de tenis y programa de entretenimiento tanto para los más jóvenes como para los mayores. Se ofrece desayuno de bufet.",
+    "capacity": "1200",
+    "floorNumber": "15",
+    "adress": "Ctra. Cuevas de Drach, S/N,PORTO CRISTO 07680",
+    "photo": "https://photos.hotelbeds.com/giata/bigger/00/001857/001857a_hb_a_013.jpg",
+    "country": "España"
+  },
+  {
+    "name": "Monteverde Boutique Hotel",
+    "description": "Monteverde Boutique Hotel tiene piscina al aire libre, jardín, salón de uso común y terraza en Pacasmayo. El alojamiento ofrece restaurante y también tiene bar. El alojamiento dispone de recepción 24 horas, traslado para ir o volver del aeropuerto, servicio de habitaciones y wifi gratis en todo el alojamiento.",
+    "capacity": "1020",
+    "floorNumber": "10",
+    "adress": " Av. alameda norte 222 - Jequetepeque, Pacasmayo",
+    "photo": "https://cf.bstatic.com/xdata/images/hotel/max1024x768/175877604.jpg?k=3a781e25c4b03409b983f3953e3507ade1b5c62c658c10547e377eace1cf0016&o=&hp=1",
+    "country": "Perú"
+  },
+  {
+    "name": "Monteverde Country Lodge",
+    "description": "Este hotel ecológico ofrece habitaciones con WiFi gratuita y vistas a los jardines tropicales en Monteverde, Costa Rica, cerca del bosque nuboso de Monteverde. Las habitaciones del Monteverde Country Lodge están ubicadas en cabañas de madera con acceso a un porche compartido. Todas cuentan con TV por cable, cafetera, caja fuerte y baño con artículos de aseo gratuitos.",
+    "capacity": "860",
+    "floorNumber": "10",
+    "adress": "Monteverde Puntarenas, 60109 Monteverde",
+    "photo": "https://cf.bstatic.com/xdata/images/hotel/max1024x768/363309263.jpg?k=40dc63146678684296f74e5803175730a3fac60268dd3a16adfdbd378261019c&o=",
+    "country": "Costa Rica"
+  },
+  {
+    "name": "Monteverde",
+    "description": "El Monteverde se encuentra en un lugar aislado de la región boscosa de Mazamitla y ofrece conexión Wi-Fi gratuita en las zonas comunes, instalaciones adaptadas para personas de movilidad reducida y gimnasio. Los alojamientos, de tipo chalé, son amplios y disponen de chimenea y zona de estar. También cuentan con terraza con vistas a los bosques, comedor y zona de cocina equipada. Todos ellos incluyen baño privado con artículos de aseo gratuitos.",
+    "capacity": "980",
+    "floorNumber": "10",
+    "adress": "Chavarria. Colonia Centro, 49500 Mazamitla",
+    "photo": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/e6/91/b7/cabanas-grandes.jpg?w=700&h=-1&s=1",
+    "country": "México"
+  },
+  {
+    "name": "Monteverde Hotel La Dorada",
+    "description": "Monteverde Hotel está en La Dorada y tiene jardín, salón de uso común, bar y wifi gratis en todo el alojamiento. El hotel tiene piscina cubierta, piscina al aire libre y terraza, además de restaurante. En el hotel, las habitaciones cuentan con armario y TV de pantalla plana. Las habitaciones del alojamiento tienen baño privado con ducha y artículos de aseo gratuitos, y algunas habitaciones también cuentan con vistas a la piscina. Todas las habitaciones de este alojamiento tienen aire acondicionado y escritorio.",
+    "capacity": "1000",
+    "floorNumber": "10",
+    "adress": "Carrera 6 # 10-43 la dorada caldas, 175031 La Dorada",
+    "photo": "https://monteverde.hotelesejecafetero.net/data/Images/1920x1080w/15009/1500966/1500966451/image-la-dorada-monteverde-hotel-3.JPEG",
+    "country": "Colombia"
+  },
+  {
+    "name": "Cabañas Monte Verde",
+    "description": "Las Cabañas Monte Verde se encuentran frente al lago Villarrica, a 5 km de Pucón, y ofrecen alojamiento independiente con calefacción y vistas al lago y a las montañas. Los bungalows de las Cabañas Monte Verde cuentan con baño privado, zona de cocina y TV. Las Cabañas Monte Verde tienen zona de barbacoa y servicio de información turística. Hay un jardín y una sala de juegos. Se puede aparcar en un parking privado gratuito de los alrededores.",
+    "capacity": "1010",
+    "floorNumber": "10",
+    "adress": "Camino Pucón - Villarrica Km 5, 4920000 Pucón",
+    "photo": "https://www.monteverdepucon.cl/wp-content/uploads/2019/01/facebook.jpg",
+    "country": "Chile"
+  }
+]
